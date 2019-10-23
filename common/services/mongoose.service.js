dde828764b227bba29dc9948c5862aa7f98be787
Rodrigo Alves <rodrigodmalves@gmail.com>
2019-10-23 16:41:54 -0300

    20191023.1641
    - Mangoose configurado
    - instalador do MangoDB pra windows

--- common/services/mongoose.service.js
+++ common/services/mongoose.service.js
@@ -7,12 +7,14 @@ const options = {
     reconnectInterval: 500, // Reconnect every 500ms
     poolSize: 10, // Maintain up to 10 socket connections
     // If not connected, return errors immediately rather than waiting for reconnect
-    bufferMaxEntries: 0
+    bufferMaxEntries: 0,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 };
 
 const connectWithRetry = () => {
     console.log('MongoDB connection with retry')
-    mongoose.connect("mongodb://mongo:27017/rest-tutorial", options).then(()=>{
+    mongoose.connect("mongodb://localhost:27017/integraFacil", options).then(()=>{
         console.log('MongoDB is connected')
     }).catch(err=>{
         console.log('MongoDB connection unsuccessful, retry after 5 seconds. ', ++count);

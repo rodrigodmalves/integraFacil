0e58ef4c6a51250c4ef763a3a7213b06ef7edc52
Rodrigo Alves <rodrigodmalves@gmail.com>
2019-11-05 16:41:25 -0300

    20191105.1640
    Inicio index.js, escutando na porta 8080

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
-console.log('- [/index.js] Iniciando aplicacao');
 const config = require('./common/config/env.config.js');
-const bodyParser = require('body-parser');
-const AuthorizationRouter = require('./authorization/routes.config');
-const UsersRouter = require('./users/routes.config');
 const express = require('express');
 const app = express();
-
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-    res.header('Access-Control-Expose-Headers', 'Content-Length');
-    res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
-    if (req.method === 'OPTIONS') {
-        return res.send(200);
-    } else {
-        return next();
-    }
-});
-
-app.use(bodyParser.json());
-AuthorizationRouter.routesConfig(app);
-UsersRouter.routesConfig(app);
 
 console.log('- [/index.js] Iniciando aplicacao - OK');
 app.listen(config.port, function () {
     console.log('- [/index.js] APP executando. Ouvindo na porta %s', config.port);
+});
+app.get('/', function(req, resp) {
+    resp.send(
+        `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1> oi oi </h1>
+                </body>
+            </html>
+        `
+    );
 });

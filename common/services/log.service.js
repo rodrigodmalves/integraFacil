a1ccb9c4dd30cd959af02439e667a9b9f97e76c0
Rodrigo Alves <rodrigodmalves@gmail.com>
2019-11-05 19:10:09 -0300

    20191105.1909
    Alinhando header e debug do motivo de não estar levantando a rota

--- common/services/log.service.js
+++ common/services/log.service.js
@@ -2,6 +2,6 @@ console.log('-- [/common/services/log.service.js] LOG - Importando modulo');
 var erro = false;
 
 exports.registrarLog = function(timestamp, integracao, mensagem,tipoRota,payload){
-    return '[data:'+timestamp+'][descIntegracao:'+integracao+'][msg:'+ mensagem +'][tipoRota:'+tipoRota +']-[payload:'+ payload +']';
+    console.log('[data:'+(new Date(timestamp))+'][descIntegracao:'+integracao+'][msg:'+ mensagem +'][tipoRota:'+tipoRota +']-[payload:'+ payload +']');
 };
 console.log('-- [/common/services/log.service.js] LOG - Importando modulo - OK');
--- common/services/rotas.service.js
+++ common/services/rotas.service.js
@@ -3,14 +3,26 @@ const log = require('../../common/services/log.service.js');
 const express = require('express');
 const app = express();
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Expose-Headers', 'Content-Length');
+    res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
+    if (req.method === 'OPTIONS') {
+        return res.send(200);
+    } else {
+        return next();
+    }
+});
+
 exports.ativarRota = function(dadosRota){
+    
     nomeIntegracao = dadosRota[0];
     tipoRest = dadosRota[1];
     conteudo = dadosRota[2];
-
     switch(tipoRest){
         case 'GET':
-            log.registrarLog(new Date(), nomeIntegracao, "Rota Acionada", tipoRest);
             app.get('/'+nomeIntegracao, function(req, resp) {
                 log.registrarLog(new Date(), nomeIntegracao, "Rota Acionada", tipoRest);
                 resp.send(
@@ -21,25 +33,22 @@ exports.ativarRota = function(dadosRota){
                     </html>`
                 );
             });
-
         case 'HEAD':
-            return log.registrarLog(new Date(), nomeIntegracao,"Tipo de Rota nao suportada",tipoRest);
+            return log.registrarLog(new Date(), nomeIntegracao,"Tipo POST ainda nao disponivel");
         case 'POST' :
-            return log.registrarLog(new Date(), nomeIntegracao,"Tipo de Rota nao suportada",tipoRest);
+            return log.registrarLog(new Date(), nomeIntegracao,"Tipo POST ainda nao disponivel");
         case 'PUT':
-            return log.registrarLog(new Date(), nomeIntegracao,"Tipo de Rota nao suportada",tipoRest);
+            return log.registrarLog(new Date(), nomeIntegracao,"Tipo POST ainda nao disponivel");
         case 'PATCH':
-            return log.registrarLog(new Date(), nomeIntegracao,"Tipo de Rota nao suportada",tipoRest);
+            return log.registrarLog(new Date(), nomeIntegracao,"Tipo POST ainda nao disponivel");
         case 'DELETE':
-            return log.registrarLog(new Date(), nomeIntegracao,"Tipo de Rota nao suportada",tipoRest);
+            return log.registrarLog(new Date(), nomeIntegracao,"Tipo POST ainda nao disponivel");
         case 'CONNECT':
-            return log.registrarLog(new Date(), nomeIntegracao,"Tipo de Rota nao suportada",tipoRest);
-        case 'OPTIONS': 
-            return log.registrarLog(new Date(), nomeIntegracao,"Tipo de Rota nao suportada",tipoRest);
+            return log.registrarLog(new Date(), nomeIntegracao,"Tipo POST ainda nao disponivel");
         case 'TRACE':
-            return log.registrarLog(new Date(), nomeIntegracao,"Tipo de Rota nao suportada",tipoRest);
+            return log.registrarLog(new Date(), nomeIntegracao,"Tipo POST ainda nao disponivel");
         default:
-            return log.registrarLog(new Date(), nomeIntegracao,"Tipo de Rota nao suportada",tipoRest);
+            return log.registrarLog(new Date(), nomeIntegracao,"Tipo de Rota nao identificada",tipoRest);
     }    
 }
 console.log('-- [/common/services/rotas.service.js] Rotas - Importando modulo - OK');

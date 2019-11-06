92b2d6c593d9e1b032ed3a1d7d2cf5672b3e50d8
Rodrigo Alves <rodrigodmalves@gmail.com>
2019-11-06 18:38:46 -0300

    20191106.1838
    Continuidade no dev de envio de dados para o endpoint final.

--- common/services/rotas.service.js
+++ common/services/rotas.service.js
@@ -2,20 +2,14 @@ const chalk = require('chalk');
 console.log(chalk.blue('--- [/common/services/rotas.service.js] Rotas - Importando modulo'));
 
 const log = require('../../common/services/log.service.js');
-const bodyParser = require('body-parser');
 const request = require('request');
 
-function conecta(configs,conteudo){
-
-    request(configs.url, { json: true }, (err, res, body) => {
-        console.log(conteudo);
-        console.log(body);
-        if (err) { 
-            return err;
-        }
-        return {statusCode: res.statusCode, resposta: res, corpo: body};
-    });
-    
+function enviaDados(configs,conteudo){
+    request(configs,(err, res, body) => {
+        if (err)
+            return {statusCode: res.statusCode, corpo: body, mensageErro: err};
+        return {statusCode: res.statusCode, corpo: body};
+    });   
 };
 
 exports.ativarRota = function(instanciaExpress,dadosRota){
@@ -24,25 +18,23 @@ exports.ativarRota = function(instanciaExpress,dadosRota){
     var tipoRest = dadosRota[1];
     var conteudo = (dadosRota[2] === undefined ?  "Sem informacoes recebidas" : dadosRota[2]);
     var endpoint = dadosRota[3];
-    
-    instanciaExpress.use(bodyParser.urlencoded({ extended: false }))
+
+    configs = {
+        url: endpoint,
+        method: tipoRest,
+        headers: {
+            'Accept': 'application/json',
+            'Accept-Charset': 'utf-8',
+            'User-Agent': 'integraFacil'
+        },
+        json: true
+    };
 
     switch(tipoRest){
         case 'GET':
-            configs = {
-                url: endpoint,
-                method: 'GET',
-                headers: {
-                    'Accept': 'application/json',
-                    'Accept-Charset': 'utf-8',
-                    'User-Agent': 'integraFacil'
-                }
-            };
-
-            instanciaExpress.get("/"+nomeIntegracao, function(req, res) {    
-                log.registrarLog("sucesso",true, new Date(), nomeIntegracao, "Rota acionada", tipoRest,req.body.Body);
-                res.json({mensagem: conecta(configs,conteudo)});
-                res.status(200).send();
+            instanciaExpress.get("/"+nomeIntegracao, function(req, res) {
+                log.registrarLog("sucesso",true, new Date(), nomeIntegracao, "Rota acionada", tipoRest);
+                res.json(enviaDados(configs,conteudo)).send().status(200);
             });
             return log.registrarLog("sucesso",false, new Date(), nomeIntegracao, "Rota liberada", tipoRest);
         

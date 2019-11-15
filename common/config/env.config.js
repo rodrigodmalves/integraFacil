0e68b4d5a9bd8f8f824c11301b894338b9d7a12c
Rodrigo Alves <rodrigodmalves@gmail.com>
2019-11-14 22:58:07 -0300

    Update env.config.js

--- common/config/env.config.js
+++ common/config/env.config.js
@@ -2,7 +2,7 @@ const chalk = require('chalk');
 console.log(chalk.blue('-- [/commom/config/env.config.js] Definindo configuracoes do servico'));
 module.exports = {
     "portaApp": process.env.PORT,
-    "portaApi": process.env.PORT,
+    "portaApi": 3000,
     "appEndpoint": "http://localhost:80/",
     "apiEndpoint": "http://localhost:9000/"
 };
